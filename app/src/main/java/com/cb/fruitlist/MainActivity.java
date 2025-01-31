652bd15c7431b479d3cefcb7e0b029b1bc54813e
Amrish Sharma <amrishsharma95@gmail.com>
2025-01-31 11:40:14 +0530

    inital commit

---
+++ app/src/main/java/com/cb/fruitlist/MainActivity.java
@@ -0,0 +1,69 @@
+package com.cb.fruitlist;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    TextToSpeech textToSpeech;
+
+    String fruitNames[] = {"Banana", "Grape", "Guava", "Mango", "Orange", "Watermelon"};
+    int fruitImageIds[] = {R.drawable.banana, R.drawable.grape, R.drawable.guava, R.drawable.mango, R.drawable.orange, R.drawable.watermelon};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+        ArrayList<HashMap<String, Object>> list = new ArrayList<>();
+        for (int i = 0; i < fruitNames.length; i++) {
+            HashMap<String, Object> map = new HashMap<>();
+            map.put("fruitName", fruitNames[i]);
+            map.put("fruitImage", fruitImageIds[i]);
+            list.add(map);
+        }
+
+        String[] from = {"fruitName", "fruitImage"};
+        int[] to = {R.id.textView, R.id.imageView};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getApplicationContext(), list, R.layout.list_row_items, from, to);
+        listView.setAdapter(simpleAdapter);
+
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(new Locale("en", "IN"));
+                }
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String fruitName = fruitNames[position];
+                textToSpeech.speak(fruitName, TextToSpeech.QUEUE_FLUSH, null, null);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
+    }
+}
